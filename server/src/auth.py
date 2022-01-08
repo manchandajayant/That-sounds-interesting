18cb871e5cacae1e7234d17e55ae0142011537a2
manchandajayant <maanjayant@gmail.com>
2022-01-09 00:46:50 +0530

    user-registration-signup

--- server/src/auth.py
+++ server/src/auth.py
@@ -1,11 +1,10 @@
 import os
-from flask import Blueprint,request
-from flask import json
-from flask.json import jsonify
+import json
+from flask import Blueprint, request
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from src.constants.status_codes import HTTP_409_CONFLICT,HTTP_400_BAD_REQUEST
-from src.services.helpers import check_email
+from src.constants.status_codes import HTTP_409_CONFLICT, HTTP_400_BAD_REQUEST,HTTP_200_OK
+from src.services.helpers import check_email,set_default
 from src.services.query import query
 
 
@@ -22,23 +21,24 @@ def register():
     password = request.json['password']
 
     # Validate Email
-    if check_email(email) is False:
-        return jsonify({'error': 'Invalid Email Format'}), HTTP_400_BAD_REQUEST
+    if check_email(email) is not True:
+        return json.dumps({'error': 'Invalid Email Format'}, default=set_default), HTTP_400_BAD_REQUEST
 
     # Validate password
     if len(password) < 6:
-        return jsonify({'error': "password is too short, it must have atleast 8 characters"}), HTTP_400_BAD_REQUEST
+        return json.dumps({'error': "password is too short,it must have atleast 8 characters"}, default=set_default), HTTP_400_BAD_REQUEST
 
     # Check if email exists
     CHECK_USER_SQL = f"SELECT id,email FROM {users_table} WHERE email='{email}'"
-    email_exists = db_query.get_data_query(users_table, CHECK_USER_SQL)
-    if email_exists is not None:
-        return jsonify({'error',"Email already exists"}), HTTP_409_CONFLICT
+    email_exists = db_query.get_data_query(CHECK_USER_SQL)
     
+    if email_exists:
+        return json.dumps({'error', "Email already exists"}, default=set_default), HTTP_409_CONFLICT
+
     # Hash the password
     password_hash = generate_password_hash(password)
 
-    # Create user 
+    # Create user
     CREATE_USER_SQL = f"INSERT INTO `users` (\
 				`username`,\
 				`email`,\
@@ -48,9 +48,11 @@ def register():
 				'{email}', '{password_hash}',\
 				CURRENT_TIMESTAMP);"
 
-    create_user = db_query.execute_query(users_table,CREATE_USER_SQL)
-    print(create_user)
-    return {"Something":"aasda"}
+    create_user = db_query.execute_query(CREATE_USER_SQL)
+
+    if create_user is None:
+        return json.dumps({"user created succesfully"},default=set_default),HTTP_200_OK
+
 
 @auth.get("/me")
 def me():
