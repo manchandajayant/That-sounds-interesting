783afb377310fb23ebce8d47bbe7e5d8a1331ac8
manchandajayant <maanjayant@gmail.com>
2022-01-09 12:54:04 +0530

    spaces file init

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 
 from src.controllers.auth.route import auth
-from src.spaces import spaces
+from src.controllers.spaces.route import spaces
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
