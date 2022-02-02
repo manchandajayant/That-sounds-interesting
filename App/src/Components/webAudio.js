14d017ed8e31b5b90a14cb8a0dd9be80328ccc19
manchandajayant <maanjayant@gmail.com>
2022-02-03 00:05:42 +0530

    convolution capabilities added through web audio api

---
+++ App/src/Components/webAudio.js
@@ -0,0 +1,30 @@
+var convolver, audioCtx, input;
+
+const getImpulseBuffer = async (audioCtx, impulseUrl) => {
+	const response = await fetch(impulseUrl);
+	const arrayBuffer = await response.arrayBuffer();
+	return audioCtx.decodeAudioData(arrayBuffer);
+};
+
+const getLiveAudio = async (audioCtx) => {
+	const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+	return audioCtx.createMediaStreamSource(stream);
+};
+
+export async function connectAudioNode() {
+	audioCtx = new AudioContext();
+	convolver = audioCtx.createConvolver();
+	input = await getLiveAudio(audioCtx);
+
+	convolver.buffer = await getImpulseBuffer(
+		audioCtx,
+		process.env.PUBLIC_URL + "/assets/ham.wav"
+	);
+	input.connect(convolver).connect(audioCtx.destination);
+}
+
+
+export function disconnectAudioNode() {
+	convolver.disconnect();
+	console.log(input.mediaStream.getAudioTracks()[0].enabled);
+}
