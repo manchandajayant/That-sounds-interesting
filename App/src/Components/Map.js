690e3770fb33c76a72461f745ac151915658ebfd
manchandajayant <maanjayant@gmail.com>
2022-02-03 14:13:49 +0530

    places added, links updated

--- App/src/Components/Map.js
+++ App/src/Components/Map.js
@@ -3,27 +3,26 @@ import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import Swal from "sweetalert2";
 import { connectAudioNode, disconnectAudioNode } from "./webAudio";
-
+import data from './data.json'
 const accessToken = process.env.REACT_APP_MAP_BOX_ACCESS_TOKEN;
 
 const Map = () => {
 	const [viewport, setViewport] = useState({
-		latitude: 51.47,
-		longitude: 0.45,
-		zoom: 1.7,
+		latitude: 40.47,
+		longitude: 3.45,
+		zoom: 1.6,
 	});
 
 	const [showPopup, togglePopup] = React.useState(false);
 	const [audioTrue, setaudioTrue] = useState(false);
-	const handleClick = () => {
+	const handleClick = (space) => {
 		var counter = 0;
 		Swal.fire({
-			imageUrl:
-				"https://www.maeshowe.co.uk/images/maeshowefisheyeint204p.jpg",
+			imageUrl: space.image,
 			imageHeight: 400,
 			imageWidth: 400,
 			imageAlt: "A tall image",
-			title: "Maes Howe",
+			title: space.name,
 			html:
 				"<div style='display:block'>" +
 				'<button type="button" role="button" tabindex="0" class="SwalBtn1 customSwalBtn"  id="btn">' +
@@ -43,11 +42,13 @@ const Map = () => {
 			// This is one of the shoddiest solutions to anything ever, must improve on this.
 			counter++;
 			if (counter % 2 !== 0) {
-				connectAudioNode();
-				document.getElementById('microphone').style.color = 'rgb(255,20,147,0.7)'
+				connectAudioNode(space);
+				document.getElementById("microphone").style.color =
+					"rgb(255,20,147,0.7)";
 			} else {
 				disconnectAudioNode();
-				document.getElementById('microphone').style.color = 'rgb(0,0,0,0.7)'
+				document.getElementById("microphone").style.color =
+					"rgb(0,0,0,0.7)";
 			}
 		};
 	};
@@ -63,16 +64,20 @@ const Map = () => {
 				onViewportChange={(nextViewport) => setViewport(nextViewport)}
 			>
 				{" "}
-				<Marker
-					latitude={37.78}
-					longitude={-77.41}
-					offsetLeft={-7}
-					offsetTop={-20}
-					captureClick={true}
-					onClick={handleClick}
-				>
-					<i className="fas fa-circle" id="marker"></i>
-				</Marker>
+				{data.map((space) => {
+					return (
+						<Marker
+							latitude={space.lat}
+							longitude={space.lng}
+							offsetLeft={-7}
+							offsetTop={-20}
+							captureClick={true}
+							onClick={() => handleClick(space)}
+						>
+							<i className="fas fa-circle" id="marker"></i>
+						</Marker>
+					);
+				})}
 				{showPopup && (
 					<Popup
 						latitude={37.78}
