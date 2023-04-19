0a6d0053a0dda212ce364b6f2a8ba6d58f29eab8
manchandajayant <maanjayant@gmail.com>
2023-04-19 17:56:40 +0530

    add places and about

--- App/src/Components/About.js
+++ App/src/Components/About.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+    return (
+        <div className="m-5 about-con">
+            <div className="m-5">
+                <p className="h-6 about">
+                    The App simulates sounds of physical places and let's you expereince them as if you were inside them. 
+                    It connects to your browser microphone. Be sure to use headphones or a sound setup
+                    to avoid a feedback loop.
+                    <br/>
+                    I am a sound artist and interested in archiving sound of physical spaces in forms of impulse
+                    responses in a common respository. Due to the lockdowns we have had over the past 2 years, I haven
+                    not been able to collect IR data around India and my city of Delhi, but i plan to do that as soon as
+                    things go back to a bit normal. 
+                    <br />
+                    <br />
+                    Impulse data from <a href="https://openairlib.net">OpenAirLib</a>, a project from the university of
+                    york and EchoThief impulse response library.
+                    <br />
+                    <br />
+                    I am currently setting up a process for anyone to be able to add an impulse response for a place to
+                    the respository. If you would like to do that now, you could mail me at jayantmanchanda1@gmail.com
+                    <br />
+                </p>
+            </div>
+        </div>
+    );
 };
 
 export default About;
